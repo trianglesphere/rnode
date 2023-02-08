c4de875d24363f527f2148b133dc8584362262b8
Joshua Gutow <jgutow@oplabs.co>
2023-02-08 16:05:13 -0600

    Parse Frames

--- src/main.rs
+++ src/main.rs
@@ -80,6 +80,57 @@ fn system_config_from_receipts(receipts: Vec<TransactionReceipt>, prev: SystemCo
 	return prev;
 }
 
+struct Frame {
+	id: H128,
+	number: u16,
+	data: Vec<u8>,
+	is_last: bool,
+}
+
+fn parse_frames(tx_data: &[u8]) -> Vec<Frame> {
+	if tx_data.len() == 0 {
+		return Vec::default();
+	} else if tx_data[0] != 0 {
+		return Vec::default();
+	}
+	let mut tx_data = &tx_data[1..];
+
+	let mut out = Vec::new();
+	loop {
+		let id = H128::from_slice(&tx_data[0..16]);
+		let number = u16::from_be_bytes(tx_data[16..18].try_into().unwrap());
+		let len = u32::from_be_bytes(tx_data[18..22].try_into().unwrap());
+		let ulen = len as usize;
+		let data = tx_data[22..22 + ulen].to_vec();
+		let is_last = tx_data[22 + ulen];
+		let is_last = if is_last == 0 {
+			false
+		} else if is_last == 1 {
+			true
+		} else {
+			return Vec::default();
+		};
+
+		tx_data = &tx_data[22 + ulen + 1..];
+
+		out.push(Frame { id, number, data, is_last });
+
+		if tx_data.len() == 0 {
+			return out;
+		}
+	}
+}
+
+fn frames_from_transactions(transactions: Vec<Transaction>) -> Vec<Frame> {
+	let batcher_address = Address::decode_hex("").unwrap();
+
+	transactions
+		.iter()
+		.filter(|tx| tx.from == batcher_address)
+		.flat_map(|tx| parse_frames(&tx.input))
+		.collect()
+}
+
 fn main() -> Result<()> {
 	// Load environment variables from local ".env" file
 	dotenv().ok();
