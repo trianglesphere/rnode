32a867d4a0883ff6db09edef3fe5a16e7a47c3ff
Andreas Bigger <abigger87@gmail.com>
2023-02-08 18:06:28 -0600

    massive refactoring

--- derivation/src/lib.rs
+++ derivation/src/lib.rs
@@ -1,70 +1,18 @@
 #![allow(dead_code)]
 
-use dotenv::dotenv;
 use ethers_core::{
 	abi::AbiDecode,
-	types::{Address, Block, Log, Transaction, TransactionReceipt, H128, H256},
+	types::{Address, Log, Transaction, TransactionReceipt, H128, H256},
 	utils::rlp::{decode, decode_list, Decodable, DecoderError, Rlp},
 };
-use ethers_providers::{Http, Middleware, Provider};
 use eyre::Result;
-use serde::{Deserialize, Serialize};
-use std::{
-	collections::{HashMap, VecDeque},
-	convert::TryFrom,
-};
-use tokio::runtime::Runtime;
-
-struct Client {
-	provider: Provider<Http>,
-	rt: Runtime,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct BlockWithReceipts {
-	block: Block<Transaction>,
-	receipts: Vec<TransactionReceipt>,
-}
-
-impl Client {
-	pub fn new(url: &str) -> Result<Self> {
-		let provider = Provider::<Http>::try_from(url)?;
-		let rt = tokio::runtime::Builder::new_current_thread().enable_all().build()?;
-
-		Ok(Client { rt, provider })
-	}
-
-	fn get_transaction_receipt(&self, transaction_hash: H256) -> Result<TransactionReceipt> {
-		let receipt = self.rt.block_on(self.provider.get_transaction_receipt(transaction_hash))?;
-
-		receipt.ok_or(eyre::eyre!("did not find the receipt"))
-	}
-
-	pub fn get_block_with_receipts(&self, hash: H256) -> Result<BlockWithReceipts> {
-		let block =
-			self.rt.block_on(self.provider.get_block_with_txs(hash))?
-				.ok_or(eyre::eyre!("did not find the block"))?;
-
-		let mut receipts = Vec::new();
-
-		for tx in block.transactions.iter() {
-			let receipt = self.get_transaction_receipt(tx.hash)?;
-			receipts.push(receipt)
-		}
-
-		Ok(BlockWithReceipts { block, receipts })
-	}
-
-	// pub fn get_head_block(&self) -> Result<Block<TxHash>, Box<dyn Error>> {
-	// 	self.provider.get_block(block_hash_or_number)
-	// }
-}
+use std::collections::{HashMap, VecDeque};
 
 // ConfigUpdateEventABI      = "ConfigUpdate(uint256,uint8,bytes)"
 // ConfigUpdateEventABIHash  = crypto.Keccak256Hash([]byte(ConfigUpdateEventABI))
 // ConfigUpdateEventVersion0 = common.Hash{}
 
-struct SystemConfig {
+pub struct SystemConfig {
 	batcher_addr: Address,
 	overhead: H256,
 	scalar: H256,
@@ -250,19 +198,4 @@ fn frames_from_transactions(transactions: Vec<Transaction>) -> Vec<Frame> {
 		.filter(|tx| tx.from == batcher_address)
 		.flat_map(|tx| parse_frames(&tx.input))
 		.collect()
-}
-
-fn main() -> Result<()> {
-	// Load environment variables from local ".env" file
-	dotenv().ok();
-
-	let provider = std::env::var("RPC")?;
-	let provider = Client::new(&provider)?;
-
-	let hash = H256::decode_hex("0xee9dd94ebc06b50d5d5c0f72299a3cc56737e459ce41ddb44f0411870f86b1a3")?;
-
-	let block = provider.get_block_with_receipts(hash)?;
-	println!("Got block: {}", serde_json::to_string_pretty(&block)?);
-
-	Ok(())
 }
