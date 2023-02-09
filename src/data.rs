17838dd9eb85f52159185ad8cf455d48a242099a
Joshua Gutow <jbgutow@gmail.com>
2023-02-09 14:14:10 -0600

    Batch Queue + Derivation Structure (#12)
    * Fix parsing multiple batches
    
    * Add batch queue struct
    
    * Derivation struct
    
    Also splits out data parsing & types into a separate file.

---
+++ src/data.rs
@@ -0,0 +1,115 @@
+use ethers_core::{
+	types::{H128, H256},
+	utils::rlp::{decode, Decodable, DecoderError, Rlp},
+};
+
+// // ConfigUpdateEventABI      = "ConfigUpdate(uint256,uint8,bytes)"
+// // ConfigUpdateEventABIHash  = crypto.Keccak256Hash([]byte(ConfigUpdateEventABI))
+// // ConfigUpdateEventVersion0 = common.Hash{}
+
+// struct SystemConfig {
+// 	batcher_addr: Address,
+// 	overhead: H256,
+// 	scalar: H256,
+// 	gas_limit: u64,
+// }
+
+// fn system_config_from_receipts(receipts: Vec<TransactionReceipt>, prev: SystemConfig) -> SystemConfig {
+// 	let l1_system_config_addr = Address::from_str("").unwrap();
+// 	let config_update_abi = H256::from_str("1d2b0bda21d56b8bd12d4f94ebacffdfb35f5e226f84b461103bb8beab6353be").unwrap();
+// 	let _logs: Vec<&Log> = receipts
+// 		.iter()
+// 		.filter(|r| r.status == Some(1.into()))
+// 		.flat_map(|r| r.logs.iter())
+// 		.filter(|l| l.address == l1_system_config_addr)
+// 		.filter(|l| l.topics.len() > 1 && l.topics[0] == config_update_abi)
+// 		.collect();
+// 	prev
+// }
+
+#[derive(Debug)]
+pub struct Frame {
+	pub id: H128,
+	pub number: u16,
+	pub data: Vec<u8>,
+	pub is_last: bool,
+}
+
+#[derive(Debug)]
+pub struct BatchV1 {
+	parent_hash: H256,
+	epoch_num: u64,
+	epoch_hash: H256,
+	timestamp: u64,
+	transactions: Vec<Vec<u8>>,
+}
+
+#[derive(Debug)]
+pub struct Batch {
+	pub batch: BatchV1,
+	// TODO: Metadata here
+}
+
+impl Decodable for Batch {
+	fn decode(rlp: &Rlp) -> Result<Self, DecoderError> {
+		// TODO: Make this more robust
+		let first = rlp.as_raw()[0];
+		if first != 0 {
+			return Err(DecoderError::Custom("invalid version byte"));
+		}
+		let batch: BatchV1 = decode(&rlp.as_raw()[1..])?;
+		Ok(Batch { batch })
+	}
+}
+
+impl Decodable for BatchV1 {
+	fn decode(rlp: &Rlp) -> Result<Self, DecoderError> {
+		let parent_hash: H256 = rlp.val_at(0)?;
+		let epoch_num: u64 = rlp.val_at(1)?;
+		let epoch_hash: H256 = rlp.val_at(2)?;
+		let timestamp: u64 = rlp.val_at(3)?;
+		let transactions: Vec<Vec<u8>> = rlp.list_at(4)?;
+
+		Ok(BatchV1 {
+			parent_hash,
+			epoch_num,
+			epoch_hash,
+			timestamp,
+			transactions,
+		})
+	}
+}
+
+// TODO: Clean this up
+pub fn parse_frames(tx_data: &[u8]) -> Vec<Frame> {
+	if tx_data.is_empty() || tx_data[0] != 0 {
+		return Vec::default();
+	}
+	let mut tx_data = &tx_data[1..];
+
+	let mut out = Vec::new();
+	loop {
+		let id = H128::from_slice(&tx_data[0..16]);
+		let number = u16::from_be_bytes(tx_data[16..18].try_into().unwrap());
+		let len = u32::from_be_bytes(tx_data[18..22].try_into().unwrap());
+		let ulen = len as usize;
+		let data = tx_data[22..22 + ulen].to_vec();
+		// dbg!(id, number, len, data.len());
+		let is_last = tx_data[22 + ulen];
+		let is_last = if is_last == 0 {
+			false
+		} else if is_last == 1 {
+			true
+		} else {
+			return Vec::default();
+		};
+
+		tx_data = &tx_data[22 + ulen + 1..];
+
+		out.push(Frame { id, number, data, is_last });
+
+		if tx_data.is_empty() {
+			return out;
+		}
+	}
+}
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@
 
 use dotenv::dotenv;
 use ethers_core::{
-	types::{Address, Log, Transaction, TransactionReceipt, H128, H256},
-	utils::rlp::{decode, Decodable, DecoderError, Rlp},
+	types::{Address, Transaction, TransactionReceipt, H128, H256},
+	utils::rlp::{decode, Rlp},
 };
 use eyre::Result;
 use flate2::read::ZlibDecoder;
@@ -21,69 +21,14 @@ pub use client::*;
 pub mod types;
 pub use types::*;
 
-// ConfigUpdateEventABI      = "ConfigUpdate(uint256,uint8,bytes)"
-// ConfigUpdateEventABIHash  = crypto.Keccak256Hash([]byte(ConfigUpdateEventABI))
-// ConfigUpdateEventVersion0 = common.Hash{}
+// Module Data
+pub mod data;
+pub use data::*;
 
-struct SystemConfig {
-	batcher_addr: Address,
-	overhead: H256,
-	scalar: H256,
-	gas_limit: u64,
-}
-
-fn system_config_from_receipts(receipts: Vec<TransactionReceipt>, prev: SystemConfig) -> SystemConfig {
-	let l1_system_config_addr = Address::from_str("").unwrap();
-	let config_update_abi = H256::from_str("1d2b0bda21d56b8bd12d4f94ebacffdfb35f5e226f84b461103bb8beab6353be").unwrap();
-	let _logs: Vec<&Log> = receipts
-		.iter()
-		.filter(|r| r.status == Some(1.into()))
-		.flat_map(|r| r.logs.iter())
-		.filter(|l| l.address == l1_system_config_addr)
-		.filter(|l| l.topics.len() > 1 && l.topics[0] == config_update_abi)
-		.collect();
-	prev
-}
-
-#[derive(Debug)]
-struct Frame {
-	id: H128,
-	number: u16,
-	data: Vec<u8>,
-	is_last: bool,
-}
-
-fn parse_frames(tx_data: &[u8]) -> Vec<Frame> {
-	if tx_data.is_empty() || tx_data[0] != 0 {
-		return Vec::default();
-	}
-	let mut tx_data = &tx_data[1..];
-
-	let mut out = Vec::new();
-	loop {
-		let id = H128::from_slice(&tx_data[0..16]);
-		let number = u16::from_be_bytes(tx_data[16..18].try_into().unwrap());
-		let len = u32::from_be_bytes(tx_data[18..22].try_into().unwrap());
-		let ulen = len as usize;
-		let data = tx_data[22..22 + ulen].to_vec();
-		// dbg!(id, number, len, data.len());
-		let is_last = tx_data[22 + ulen];
-		let is_last = if is_last == 0 {
-			false
-		} else if is_last == 1 {
-			true
-		} else {
-			return Vec::default();
-		};
-
-		tx_data = &tx_data[22 + ulen + 1..];
-
-		out.push(Frame { id, number, data, is_last });
-
-		if tx_data.is_empty() {
-			return out;
-		}
-	}
+#[derive(Default)]
+struct Channel {
+	frames: HashMap<u16, Frame>,
+	// TODO: Size + handling insertion of frames
 }
 
 #[derive(Default)]
@@ -91,12 +36,6 @@ struct ChannelBank {
 	channels_map: HashMap<H128, Channel>,
 	channels_by_creation: VecDeque<H128>,
 	// TODO: Pruning
-}
-
-#[derive(Default)]
-struct Channel {
-	frames: HashMap<u16, Frame>,
-	// TODO: Size + handling insertion of frames
 }
 
 impl Channel {
@@ -157,48 +96,18 @@ impl ChannelBank {
 	}
 }
 
-#[derive(Debug)]
-struct BatchV1 {
-	parent_hash: H256,
-	epoch_num: u64,
-	epoch_hash: H256,
-	timestamp: u64,
-	transactions: Vec<Vec<u8>>,
+#[derive(Default)]
+pub struct BatchQueue {
+	l1_blocks: VecDeque<BlockWithReceipts>, // TODO: Block ID here
+	// Map batch timestamp to batches in order that they were received
+	batches: HashMap<u64, VecDeque<Batch>>,
 }
 
-impl Decodable for BatchV1 {
-	fn decode(rlp: &Rlp) -> Result<Self, DecoderError> {
-		let parent_hash: H256 = rlp.val_at(0)?;
-		let epoch_num: u64 = rlp.val_at(1)?;
-		let epoch_hash: H256 = rlp.val_at(2)?;
-		let timestamp: u64 = rlp.val_at(3)?;
-		let transactions: Vec<Vec<u8>> = rlp.list_at(4)?;
-
-		Ok(BatchV1 {
-			parent_hash,
-			epoch_num,
-			epoch_hash,
-			timestamp,
-			transactions,
-		})
-	}
-}
-
-#[derive(Debug)]
-struct Batch {
-	batch: BatchV1,
-	// TODO: Metadata here
-}
-
-impl Decodable for Batch {
-	fn decode(rlp: &Rlp) -> Result<Self, DecoderError> {
-		// TODO: Make this more robust
-		let first = rlp.as_raw()[0];
-		if first != 0 {
-			return Err(DecoderError::Custom("invalid version byte"));
+impl BatchQueue {
+	pub fn load_batches(&mut self, batches: Vec<Batch>, _l1_origin: &Header) {
+		for b in batches {
+			println!("{b:?}");
 		}
-		let batch: BatchV1 = decode(&rlp.as_raw()[1..])?;
-		Ok(Batch { batch })
 	}
 }
 
@@ -209,17 +118,27 @@ fn channel_bytes_to_batches(data: Vec<u8>) -> Vec<Batch> {
 	// TODO: Handle this error
 	// Decompress the passed data with zlib
 	decomp.read_to_end(&mut buffer).unwrap();
+	let mut buf: &[u8] = &buffer;
 
 	// TODO: Truncate data to 10KB (post compression)
 	// The data we received is an RLP encoded string. Before decoding the batch itself,
 	// we need to decode the string to get the actual batch data.
-	let decoded_batch = decode::<Vec<u8>>(&buffer).unwrap();
-	// Decode the batch itself.
-	let b: Batch = decode(&decoded_batch).unwrap();
+	let mut decoded_batches: Vec<Vec<u8>> = Vec::new();
+	loop {
+		let rlp = Rlp::new(buf);
+		let size = rlp.size();
 
-	dbg!(&b);
+		match rlp.as_val() {
+			Ok(b) => {
+				decoded_batches.push(b);
+				buf = &buf[size..];
+			}
+			Err(_) => break,
+		}
+	}
+	// dbg!(decoded_batches);
 
-	vec![b]
+	decoded_batches.iter().map(|b| decode(b)).filter_map(|b| b.ok()).collect()
 }
 
 fn frames_from_transactions(transactions: Vec<Transaction>) -> Vec<Frame> {
@@ -232,29 +151,40 @@ fn frames_from_transactions(transactions: Vec<Transaction>) -> Vec<Frame> {
 		.collect()
 }
 
+#[derive(Default)]
+struct Derivation {
+	channel_bank: ChannelBank,
+	batch_queue: BatchQueue,
+}
+
+impl Derivation {
+	pub fn load_l1_data(&mut self, header: Header, transactions: Vec<Transaction>, _receipts: Vec<TransactionReceipt>) {
+		let frames = frames_from_transactions(transactions);
+		self.channel_bank.load_frames(frames);
+		let mut batches = Vec::new();
+		while let Some(data) = self.channel_bank.get_channel_data() {
+			let mut b = channel_bytes_to_batches(data);
+			batches.append(&mut b);
+		}
+		self.batch_queue.load_batches(batches, &header);
+	}
+
+	pub fn next_l2_attributes(_l2_head: Header) -> Header {
+		todo!()
+	}
+}
+
 fn main() -> Result<()> {
 	// Load environment variables from local ".env" file
 	dotenv().ok();
 
 	let provider = std::env::var("RPC")?;
 	let mut provider = Client::new(&provider)?;
-
 	let hash = H256::from_str("0x20ffc57ae0c607d4b612662251738b01c44f8a9a42a1da89a881a56a5fad426e")?;
-
 	let block = provider.get_block_with_receipts(hash)?;
-	// println!("Got block: {}", serde_json::to_string_pretty(&block)?);
 
-	let frames = frames_from_transactions(block.block.transactions);
-	let mut cb = ChannelBank::default();
-	cb.load_frames(frames);
-	let channel_data = cb.get_channel_data();
-
-	if let Some(d) = channel_data {
-		let batches = channel_bytes_to_batches(d);
-		println!("Got batches: {batches:?}");
-	} else {
-		println!("Invalid batch")
-	}
+	let mut derivation = Derivation::default();
+	derivation.load_l1_data(Header::default(), block.block.transactions, block.receipts);
 
 	Ok(())
 }
