11491d0fc2863e204319123b20b76b4f10cbd89a
Joshua Gutow <jbgutow@gmail.com>
2023-03-16 10:17:49 -0700

    Add go code

--- go-mpt/main.go
+++ go-mpt/main.go
@@ -3,21 +3,23 @@ package main
 import (
 	"fmt"
 
-	"github.com/davecgh/go-spew/spew"
-	"github.com/ethereum/go-ethereum/trie"
+	// "github.com/davecgh/go-spew/spew"
 	"github.com/ethereum/go-ethereum/core/rawdb"
+	"github.com/ethereum/go-ethereum/ethdb/memorydb"
+	"github.com/ethereum/go-ethereum/trie"
 )
 
 func main() {
 	// ('do', 'verb'), ('dog', 'puppy'), ('doge', 'coin'), ('horse', 'stallion').
-	db := trie.NewDatabase(rawdb.NewMemoryDatabase())
+	raw := memorydb.New()
+	db := trie.NewDatabase(rawdb.NewDatabase(raw))
 	t := trie.NewEmpty(db)
 	t.Update([]byte("do"), []byte("verb"))
 	t.Update([]byte("dog"), []byte("puppy"))
 	// t.Update([]byte("doge"), []byte("coin"))
 	// t.Update([]byte("horse"), []byte("stallion"))
-	hash, nodes := t.Commit(true)
+	hash := t.Hash()
 	fmt.Println(db.Node(hash))
-	fmt.Println(hash)
-	spew.Dump(nodes)
+	// hash, nodes := t.Commit(false)
+	// spew.Dump(nodes)
 }
