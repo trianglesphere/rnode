1bfd853d61f9b23dc13139cd500f02525a7305ae
Joshua Gutow <jbgutow@gmail.com>
2023-04-20 16:33:54 -0700

    derivation: Run over multiple L1 blocks
    Also includes some cleanup code.

--- crates/derivation/src/batch_queue.rs
+++ crates/derivation/src/batch_queue.rs
@@ -1,5 +1,4 @@
 use ethers_core::{types::Transaction, utils::rlp::decode};
-use std::collections::hash_map::Entry;
 use std::collections::{HashMap, VecDeque};
 
 use super::batch::Batch;
@@ -29,27 +28,25 @@ impl BatchQueue {
 	pub fn load_batches(&mut self, batches: impl Iterator<Item = Batch>, l1_origin: L1BlockRef) {
 		self.l1_blocks.push_back(l1_origin);
 		for b in batches {
-			println!("{b:?}");
-			if let Entry::Vacant(e) = self.batches.entry(b.batch.timestamp) {
-				e.insert(VecDeque::default());
-			}
-			self.batches.get_mut(&b.batch.timestamp).unwrap().push_back(b);
+			self.batches.entry(b.batch.timestamp).or_default().push_back(b);
 		}
 	}
 
 	pub fn get_block_candidate(&mut self, l2_head: L2BlockRef) -> Option<L2BlockCandidate> {
 		let next_timestamp = l2_head.time + self.l2_block_time;
 		if let Some(candidates) = self.batches.get(&next_timestamp) {
-			let out = candidates.front().expect("Should have entry in any created queue");
-			// TODO: Throw out the batch if we can't decode it.
-			let txns = out.batch.transactions.iter().map(|t| decode::<Transaction>(t).unwrap()).collect();
-			self.batches.remove(&next_timestamp);
-			// TODO: deposits, seq number, transactions from batches
-			return Some(L2BlockCandidate {
-				number: l2_head.number + 1,
-				timestamp: next_timestamp,
-				transactions: txns,
-			});
+			#[allow(clippy::never_loop)]
+			for b in candidates {
+				// TODO: Do this step earlier
+				let txns = b.batch.transactions.iter().map(|t| decode::<Transaction>(t).unwrap()).collect();
+				self.batches.remove(&next_timestamp);
+				// TODO: deposits, seq number, transactions from batches
+				return Some(L2BlockCandidate {
+					number: l2_head.number + 1,
+					timestamp: next_timestamp,
+					transactions: txns,
+				});
+			}
 		}
 
 		None
--- rnode/src/main.rs
+++ rnode/src/main.rs
@@ -2,7 +2,6 @@ use dotenv::dotenv;
 use eyre::Result;
 
 use client::prelude::*;
-use core::{hash_literal, types::Hash};
 use derivation::derivation::Derivation;
 
 fn main() -> Result<()> {
@@ -11,14 +10,9 @@ fn main() -> Result<()> {
 
 	let provider = std::env::var("RPC")?;
 	let mut provider = Client::new(&provider)?;
-	let hash = hash_literal!("20ffc57ae0c607d4b612662251738b01c44f8a9a42a1da89a881a56a5fad426e");
-
-	let header = provider.get_header(hash)?;
-	let transactions = provider.get_transactions_by_root(header.transactions_root.into())?;
-	let receipts = provider.get_receipts_by_root(header.receipts_root.into())?;
 
 	let mut derivation = Derivation::new(core::chain_config::GOERLI_CONFIG);
-	derivation.load_l1_data(header.into(), transactions, receipts);
+	derivation.run(8300532, 8300533, &mut provider);
 
 	Ok(())
 }
